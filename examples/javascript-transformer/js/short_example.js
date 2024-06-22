faa05b8bcfc797fc3417589d5ef11964ee903278
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-06-23 00:34:04 +0200

    added examples

---
+++ examples/javascript-transformer/js/short_example.js
@@ -0,0 +1,13 @@
+res = {}
+
+res.title = x.preTransformed.datasetVersion.metadataBlocks.citation.title
+
+res.author = new List()
+for (i = 0; i < x.preTransformed.datasetVersion.metadataBlocks.citation.author.length; i++) {
+  res.author.add(x.preTransformed.datasetVersion.metadataBlocks.citation.author[i].authorName[0])
+}
+
+res.files = new List()
+for (i = 0; i < x.datasetSchemaDotOrg.distribution.length; i++) {
+  res.files.add(x.datasetSchemaDotOrg.distribution[i].contentUrl)
+}
