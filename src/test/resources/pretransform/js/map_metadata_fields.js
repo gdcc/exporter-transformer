0547363843ac11ed3bfa5c6b484cf43f9407ac06
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2024-07-08 17:33:12 +0200

    pretransformation bugfixes

--- src/test/resources/pretransform/js/map_metadata_fields.js
+++ src/test/resources/pretransform/js/map_metadata_fields.js
@@ -1,23 +1,33 @@
-mapSubField = function (subField) {
+mapSubField = function (subField, multiple) {
     if (typeof subField === 'string') {
         return subField
     }
-    var mappedSubField = {}
+    var resArray = new List()
     subField.forEach(function (value) {
         if (value.keySet) {
+            var mappedSubField = {}
             value.keySet().forEach(function (key) {
-                var mapped = mapSubField(value.get(key).value)
-                mappedSubField[key] = mappedSubField[key] ? mappedSubField[key].add(mapped) : new List([mapped])
+                var mapped = mapSubField(value.get(key).value, value.get(key).multiple)
+                if (!value.get(key).multiple) {
+                    mappedSubField[key] = mapped
+                } else {
+                    mappedSubField[key] = mappedSubField[key] ? mappedSubField[key].add(mapped) : new List([mapped])
+                }
             })
+            resArray.add(mappedSubField)
         } else {
-            mappedSubField = subField
+            resArray.add(value)
         }
     })
-    return mappedSubField
+    return multiple ? resArray : resArray[0]
 }
 
 res = {}
 x.stream().forEach(function (field) {
-    var mapped = mapSubField(field.value)
-    res[field.typeName] = res[field.typeName] ? res[field.typeName].add(mapped) : new List([mapped])
+    var mapped = mapSubField(field.value, field.multiple)
+    if (!field.multiple) {
+        res[field.typeName] = mapped
+    } else {
+        res[field.typeName] = res[field.typeName] ? res[field.typeName].addAll(mapped) : mapped
+    }
 })
