22c5ae298c98694f8f3f6fb65310983b541e29b0
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2024-07-10 10:34:42 +0200

    reverted removal of language detection in ddi-pdf example

--- examples/ddi-pdf/transformer.py
+++ examples/ddi-pdf/transformer.py
@@ -6,13 +6,14 @@ from javax.xml.transform.sax import SAXResult
 from javax.xml.transform.stream import StreamSource
 from org.apache.fop.apps import FopFactory, MimeConstants
 
-# localeEnvVar = System.getenv().get("LANG") if System.getenv().get("LANG") else "en"
-# if localeEnvVar.index(".") > 0:
-#     localeEnvVar = localeEnvVar[0 : localeEnvVar.index(".")]
-# if localeEnvVar.index("_") > 0:
-#    localeEnvVar = localeEnvVar[0 : localeEnvVar.index("_")]
-
-localeEnvVar = "en"
+localeEnvVar = System.getenv().get("LANG") if System.getenv().get("LANG") else "en"
+print(localeEnvVar)
+if localeEnvVar.index(".") > 0:
+    localeEnvVar = localeEnvVar[0 : localeEnvVar.index(".")]
+if localeEnvVar.index("_") > 0:
+    localeEnvVar = localeEnvVar[0 : localeEnvVar.index("_")]
+if localeEnvVar == "C":
+    localeEnvVar = "en"
 
 fopFactory = FopFactory.newInstance(File(".").toURI())
 foUserAgent = fopFactory.newFOUserAgent()
